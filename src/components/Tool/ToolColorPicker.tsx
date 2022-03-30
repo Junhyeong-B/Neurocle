f3c9987ac2eeee69711343e1884f512df4354b97
JunHyeongB <aowlr0513@gmail.com>
2022-03-30 18:12:54 +0900

    design: ColorPicker 컴포넌트 color 기본값 hex 길이 6으로 기본값 설정

--- src/components/Tool/ToolColorPicker.tsx
+++ src/components/Tool/ToolColorPicker.tsx
@@ -5,12 +5,15 @@ type Props = {
   onColorChange?(color: string): void;
 };
 
-const ColorPicker = ({ color = "#000", onColorChange }: Props): JSX.Element => {
+const ColorPicker = ({
+  color = "#000000",
+  onColorChange,
+}: Props): JSX.Element => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     onColorChange && onColorChange(e.target.value);
   };
 
-  return <input type="color" value={color} onChange={handleChange} />;
+  return <input type="color" value={color} onChange={handleChange} readOnly />;
 };
 
 export default React.memo(ColorPicker);
