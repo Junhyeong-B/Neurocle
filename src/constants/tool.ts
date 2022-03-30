1c9d3d2607f188c32b35ad65e44c0d57591c500a
JunHyeongB <aowlr0513@gmail.com>
2022-03-30 17:41:08 +0900

    feat: 그리기 도구(Tool) 상수 추가

---
+++ src/constants/tool.ts
@@ -0,0 +1,39 @@
+export const STRAIGHT = "STRAIGHT";
+export const CURVE = "CURVE";
+export const CIRCLE = "CIRCLE";
+export const RECTANGLE = "RECTANGLE";
+export const POLYGON = "POLYGON";
+export const CLEAR = "CLEAR";
+
+export const DRAWING_TOOL = [
+  {
+    id: 0,
+    type: STRAIGHT,
+    name: "직선",
+  },
+  {
+    id: 1,
+    type: CURVE,
+    name: "곡선",
+  },
+  {
+    id: 2,
+    type: CIRCLE,
+    name: "원",
+  },
+  {
+    id: 3,
+    type: RECTANGLE,
+    name: "직사각형",
+  },
+  {
+    id: 4,
+    type: POLYGON,
+    name: "다각형",
+  },
+  {
+    id: 5,
+    type: CLEAR,
+    name: "클리어",
+  },
+] as const;
