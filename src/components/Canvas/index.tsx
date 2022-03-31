5f963ce8c3b50ed100f3dec09a5ce4f4b81f98ae
JunHyeongB <aowlr0513@gmail.com>
2022-04-01 00:51:23 +0900

    refactor: Canvas 컴포넌트 중복 코드 제거, 함수 useCallback 적용

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,5 +1,5 @@
 import { KonvaEventObject } from "konva/lib/Node";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { Stage, Layer, Path } from "react-konva";
 import {
   CIRCLE,
@@ -40,118 +40,113 @@ const Canvas = (): JSX.Element => {
   const [paths, setPaths] = useState<PathsType[]>([]);
   const isDrawing = useRef<boolean>(false);
 
-  const handleMouseDown = (e: KonvaEventObject<MouseEvent>) => {
-    isDrawing.current = true;
-    const stage = e.target.getStage()!;
-    const point = stage.getPointerPosition()!;
+  const handleMouseDown = useCallback(
+    (e: KonvaEventObject<MouseEvent>) => {
+      isDrawing.current = true;
+      const stage = e.target.getStage()!;
+      const point = stage.getPointerPosition()!;
 
-    setPaths([...paths, { tool, points: [point.x, point.y], color, stroke }]);
-  };
+      setPaths((prevPaths) => [
+        ...prevPaths,
+        { tool, points: [point.x, point.y], color, stroke },
+      ]);
+    },
+    [tool, color, stroke],
+  );
 
-  const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
-    if (!isDrawing.current) {
-      return;
-    }
+  const handleMouseMove = useCallback(
+    (e: KonvaEventObject<MouseEvent>) => {
+      if (!isDrawing.current) {
+        return;
+      }
 
-    const stage = e.target.getStage()!;
-    const point = stage.getPointerPosition()!;
-    const lastPath = paths[paths.length - 1];
-    const [startX, startY] = [lastPath.points[0], lastPath.points[1]];
+      const stage = e.target.getStage()!;
+      const point = stage.getPointerPosition()!;
+      const lastPath = paths[paths.length - 1];
+      const [startX, startY] = [lastPath.points[0], lastPath.points[1]];
 
-    switch (tool) {
-      case STRAIGHT:
-        const straightPathData = getStraightPath({
-          startX,
-          startY,
-          endX: point.x,
-          endY: point.y,
-        });
-        lastPath.data = straightPathData;
+      switch (tool) {
+        case STRAIGHT:
+          const straightPathData = getStraightPath({
+            startX,
+            startY,
+            endX: point.x,
+            endY: point.y,
+          });
+          lastPath.data = straightPathData;
+          break;
+        case CURVE:
+          const curvePath = getCurvePath({
+            startX,
+            startY,
+            x: point.x,
+            y: point.y,
+            path: lastPath.curvePath,
+          });
+          lastPath.curvePath = curvePath;
+          lastPath.data = curvePath.pathData.join(" ");
+          break;
+        case CIRCLE:
+          const radius = getRadius(startX, startY, point.x, point.y);
+          const circlePathData = getCirclePath({
+            centerX: startX,
+            centerY: startY,
+            radius,
+          });
+          lastPath.data = circlePathData;
+          break;
+        case RECTANGLE:
+          const rectanglePathData = getRectanglePath({
+            startX,
+            startY,
+            x: point.x,
+            y: point.y,
+          });
+          lastPath.data = rectanglePathData;
+          break;
+        case POLYGON:
+          const size = getRadius(startX, startY, point.x, point.y) * 2;
+          const polygonPathData = getPolygonPath({
+            centerX: startX,
+            centerY: startY,
+            size,
+            sides: stroke,
+          });
+          lastPath.data = polygonPathData;
+          break;
+        default:
+      }
 
-        [lastPath.points[2], lastPath.points[3]] = [point.x, point.y];
+      paths.splice(paths.length - 1, 1, lastPath);
+      setPaths(paths.concat());
+    },
+    [paths, stroke, tool],
+  );
 
-        paths.splice(paths.length - 1, 1, lastPath);
-        setPaths(paths.concat());
-        break;
-      case CURVE:
-        const curvePath = getCurvePath({
-          startX,
-          startY,
-          x: point.x,
-          y: point.y,
-          path: lastPath.curvePath,
-        });
-        lastPath.curvePath = curvePath;
-        lastPath.data = curvePath.pathData.join(" ");
-
-        paths.splice(paths.length - 1, 1, lastPath);
-        setPaths(paths.concat());
-        break;
-      case CIRCLE:
-        const radius = getRadius(startX, startY, point.x, point.y);
-        const circlePathData = getCirclePath({
-          centerX: startX,
-          centerY: startY,
-          radius,
-        });
-        lastPath.data = circlePathData;
-
-        paths.splice(paths.length - 1, 1, lastPath);
-        setPaths(paths.concat());
-        break;
-      case RECTANGLE:
-        const rectanglePathData = getRectanglePath({
-          startX,
-          startY,
-          x: point.x,
-          y: point.y,
-        });
-        lastPath.data = rectanglePathData;
-
-        paths.splice(paths.length - 1, 1, lastPath);
-        setPaths(paths.concat());
-        break;
-      case POLYGON:
-        const size = getRadius(startX, startY, point.x, point.y) * 2;
-        const polygonPathData = getPolygonPath({
-          centerX: startX,
-          centerY: startY,
-          size,
-          sides: stroke,
-        });
-        lastPath.data = polygonPathData;
-
-        paths.splice(paths.length - 1, 1, lastPath);
-        setPaths(paths.concat());
-        break;
-      default:
-    }
-  };
-
-  const handleMouseUp = () => {
+  const handleMouseUp = useCallback(() => {
     isDrawing.current = false;
-  };
+  }, []);
 
-  const handleMouseLeave = () => {
+  const handleMouseLeave = useCallback(() => {
     isDrawing.current = false;
-  };
+  }, []);
 
-  const handleColorChange = (color: string) => {
+  const handleColorChange = useCallback((color: string) => {
     setColor(color);
-  };
+  }, []);
 
-  const handleStrokeChange = (stroke: number) => {
+  const handleStrokeChange = useCallback((stroke: number) => {
     setStroke(stroke);
-  };
+  }, []);
 
-  const handleChangeTool = (tool: ToolType) => {
+  const handleChangeTool = useCallback((tool: ToolType) => {
     if (tool === CLEAR) {
       setPaths([]);
       return;
     }
 
     setTool(tool);
-  };
+  }, []);
 
   return (
     <div>
